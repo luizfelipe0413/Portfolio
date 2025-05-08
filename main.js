b9c91c0b98ba661e571076ec9e19c265add05b41
Lillia <luizkadu20@gmail.com>
2025-05-08 20:11:49 -0300

    Texto novo na main page e polimento da pagina de contato

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-const lightModeIcon = document.getElementById('lightmode');
-const darkModeIcon = document.getElementById('darkmode');
-
-lightModeIcon.addEventListener('click', () => {
-    lightModeIcon.classList.add('clicked');
-    darkModeIcon.classList.add('clicked');
-});
-
-darkModeIcon.addEventListener('click', () => {
-    lightModeIcon.classList.remove('clicked');
-    darkModeIcon.classList.remove('clicked');
-});
-
 const menuIcon = document.querySelector('#menu img')
 const nav = document.querySelector('#menu nav')
+const textonome = document.querySelector('header h1')
 
 menuIcon.addEventListener('click', () => {
     nav.classList.toggle('active')
     menuIcon.classList.toggle('rotate')
+    textonome.classList.toggle('active')
 })
 
 const emailcopy = document.querySelector('#email')
@@ -24,10 +13,24 @@ const emailcopy = document.querySelector('#email')
 emailcopy.addEventListener('click', () => {
     navigator.clipboard.writeText(emailcopy.textContent)
     .then(() => {
-        alert('Email copiado com sucesso !')
+        alert('Email copiado com sucesso.')
     })
     .catch(falha => {
-        alert('Falha ao copiar o email !')
+        alert('Falha ao copiar o email.')
     })
 
 })
+
+
+// const lightModeIcon = document.getElementById('lightmode');
+// const darkModeIcon = document.getElementById('darkmode');
+
+// lightModeIcon.addEventListener('click', () => {
+//     lightModeIcon.classList.add('clicked');
+//     darkModeIcon.classList.add('clicked');
+// });
+
+// darkModeIcon.addEventListener('click', () => {
+//   darkModeIcon.classList.remove('clicked');
+// });     lightModeIcon.classList.remove('clicked');
+//  
